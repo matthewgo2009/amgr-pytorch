89ea9937893434dfdde6486a09b3479d23459661
matthewgo2009 <matthewgo2009@gmail.com>
2023-06-22 15:41:10 -0700

    Add files via upload

--- main_amgr.py
+++ main_amgr.py
@@ -57,7 +57,7 @@ def main():
 
         return
 
-    args.logit_adjustments = utils.compute_adjustment(train_loader, args.tro_train, args)
+    # args.logit_adjustments = utils.compute_adjustment(train_loader, args.tro_train, args)
 
     optimizer = torch.optim.SGD(model.parameters(),
                                 args.lr,
@@ -72,7 +72,7 @@ def main():
     for epoch in loop:
 
         # train for one epoch
-        train_loss, train_acc = train(train_loader, model, criterion, optimizer)
+        train_loss, train_acc = train(train_dataset, model, criterion, optimizer,num_train,gamma)
         writer.add_scalar("train/acc", train_acc, epoch)
         writer.add_scalar("train/loss", train_loss, epoch)
         lr_scheduler.step()
@@ -99,21 +99,44 @@ def main():
     writer.add_hparams(hparam_dict=hyper_param, metric_dict=results)
     writer.close()
 
-def q(model,x_i,y_i,x_j,y_j,gamma):
-    output_i = model(x_i)
-    loss_i = criterion(output_i, y_i)
-    loss_i.backward()
-    para = model.parameters()
-    grad_i = para.grad
-    grad_i.flatten()
+
+def compute_grad(sample, target, criterion, model):
+    
+    sample = sample.unsqueeze(0)  # prepend batch dimension for processing
+    target = target.unsqueeze(0)
+
+    prediction = model(sample)
+    loss = criterion(prediction, target)
+
+    grad = torch.autograd.grad(loss, list(model.parameters()))
+
+    flat_grad = torch.tensor([])
+    for item in grad:
+        flat_grad = torch.cat((flat_grad,item.flatten()), dim=0)
+    # flat_grad = torch.stack([item.flatten() for item in grad])
+    return flat_grad
+
+def q(model,criterion,x_i,y_i,x_j,y_j,gamma):
+    # x_i = x_i.unsqueeze(0)
+    # y_i = y_i.unsqueeze(0)
+
+    # output_i = model(x_i)
+    # loss_i = criterion(output_i, y_i)
+    # loss_i.backward() 
+    # grad_i = torch.autograd.grad(loss_i, list(model.parameters()))
+    grad_i = compute_grad(x_i, y_i,criterion, model)
     grad_i = grad_i/np.linalg.norm(grad_i)
 
-    output_j = model(x_j)
-    loss_j = criterion(output_j, y_j)
-    loss_j.backward()
-    para = model.parameters()
-    grad_j = para.grad
-    grad_j.flatten()
+    # x_j = x_j.unsqueeze(0)
+    # y_j = y_j.unsqueeze(0)
+    # output_j = model(x_j)
+    # loss_j = criterion(output_j, y_j)
+    # loss_j.backward()
+    # grad_j = torch.autograd.grad(loss_j, list(model.parameters()))
+
+    grad_j = compute_grad(x_j, y_j, criterion,model)
+
+
     grad_j = grad_j/np.linalg.norm(grad_j)
 
 
@@ -121,72 +144,81 @@ def q(model,x_i,y_i,x_j,y_j,gamma):
 
 
 
-def train(train_dataset, model, criterion, optimizer):
+def train(train_dataset, model, criterion, optimizer,num_train,gamma):
     """ Run one train epoch """
 
     losses = utils.AverageMeter()
     accuracies = utils.AverageMeter()
 
     model.train()
-    num_batches = num_train/args.batch_size
-    arr = np.arange(num_train)
-    arr1 = random.shuffle(arr)
-    arr2 = random.shuffle(arr)
+    old_model=model
+    num_batches = int(num_train/args.batch_size)
+    arr1 = np.arange(num_train)
+    arr2 = np.arange(num_train)
+ 
+    np.random.shuffle(arr1)
+    np.random.shuffle(arr2)
     for t in range(num_batches):
-    # for _, (inputs, target) in enumerate(train_loader):
-        B1, Y1= train_dataset[arr1[t]*batch_size:(arr1[t]+1)*batch_size]
+        batch = [train_dataset[i] for i in arr1[t*args.batch_size:(t+1)*args.batch_size]]
+        B1 = list(zip(*batch))[0] 
+        B1 =  torch.stack(B1)
+        Y1 = list(zip(*batch))[1] 
+        Y1 = torch.tensor(Y1)        
         Y1 = Y1.to(device)
-        print(Y1.shape)
         B1_var = B1.to(device)
         Y1_var = Y1
-        print('minibatch size is:'+str(B1_var.shape))
+ 
 
-
-        B2, Y2= train_dataset[arr2[t]*batch_size:(arr2[t]+1)*batch_size]
+        batch = [train_dataset[i] for i in arr2[t*args.batch_size:(t+1)*args.batch_size]]
+        B2 = list(zip(*batch))[0]
+        B2 =  torch.stack(B2)
+        Y2 = list(zip(*batch))[1]  
+        Y2 = torch.tensor(Y2)       
         Y2 = Y2.to(device)
         B2_var = B2.to(device)
         Y2_var = Y2
  
-        output = model(input_var)
-        acc = utils.accuracy(output.data, target)
-        print('Output dimension is:'+str(output.shape))
-        # if args.logit_adj_train:
-        #     output = output + args.logit_adjustments
-
-        loss_r = 0
-        for parameter in model.parameters():
-            loss_r += torch.sum(parameter ** 2)
-        loss = loss + args.weight_decay * loss_r
+        
 
 
-         #####update z to approx exp of sum #######
-        for i in range(arr1[t]*batch_size:(arr1[t]+1)*batch_size):
-            x_i,y_i = train_dataset[i]
+        #####update z to approx exp of sum #######
+        weight = []
+        for i in range(len(B1)):
+            x_i,y_i = B1[i], Y1[i]
             corr = 0
-            for j in range(arr2[t]*batch_size:(arr2[t]+1)*batch_size)
-                x_j,y_j = train_dataset[j]
-                corr = corr + q(model, x_i,y_i,x_j,y_j,gamma) - q(old_model, x_i,y_i,x_j,y_j,gamma) 
+            for j in range(len(B2)):
+                x_j,y_j = B2[j], Y2[j]
+                corr = corr + q(model,criterion, x_i,y_i,x_j,y_j,gamma) - q(old_model,criterion, x_i,y_i,x_j,y_j,gamma) 
             z[i] = (1-beta)*(z[i]+corr1) + beta*corr1
+            weight.append(math.exp(-z[i]))
 
 
         #####compute stochastic gradients#######
 
         old_model = model 
-        weighted_grad = np.zeros(model.parameters().shape)
+
+        output = model(B1_var)
+        acc = utils.accuracy(output.data, Y1_var) 
+        loss = criterion(output, Y1_var, weight=weight)
+
+        loss_r = 0
+        for parameter in model.parameters():
+            loss_r += torch.sum(parameter ** 2)
+        loss = loss + args.weight_decay * loss_r
         
-        for i in range(arr1[t]*batch_size:(arr1[t]+1)*batch_size):
-            x_i,y_i = train_dataset[i]
-            output_i = model(x_i)
-            loss_i = criterion(output_i, y_i)
-            loss_i.backward()
-            para = model.parameters()
-            grad_i = para.grad
-            weighted_grad = weighted_grad + math.exp(-z[i])*grad_i 
+        # for i in range(arr1[t]*batch_size, (arr1[t]+1)*batch_size):
+        #     x_i,y_i = train_dataset[i]
+        #     output_i = model(x_i)
+        #     loss_i = criterion(weight=weight,output_i, y_i)
+        #     loss_i.backward()
+        #     para = model.parameters()
+        #     grad_i = para.grad
+        #     weighted_grad = weighted_grad + math.exp(-z[i])*grad_i 
  
-        model.parameters() = model.parameters() - eta* weighted_grad
-        # optimizer.zero_grad()
-        # loss.backward()
-        # optimizer.step()
+        # model.parameters() = model.parameters() - eta*weighted_grad
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
         losses.update(loss.item(), inputs.size(0))
         accuracies.update(acc, inputs.size(0))
