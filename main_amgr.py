e11da243d49cd865b11f584f4ee07b27f1b206e2
matthewgo2009 <matthewgo2009@gmail.com>
2023-06-30 15:09:51 -0700

    Add files via upload

--- main_amgr.py
+++ main_amgr.py
@@ -228,7 +228,7 @@ def train(train_dataset, model, criterion, optimizer,num_train,gamma,z,epoch):
         
 
         weighted_loss = torch.inner(loss,weight)
-        print(weighted_loss)
+        # print(weighted_loss)
         loss = loss.mean()
         loss_r = 0
         for parameter in model.parameters():
