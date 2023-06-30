341dae2568b0e463d6174423fbcfa8b13ef4d8ec
matthewgo2009 <matthewgo2009@gmail.com>
2023-06-30 11:15:38 -0700

    Add files via upload

--- main_amgr.py
+++ main_amgr.py
@@ -194,8 +194,7 @@ def train(train_dataset, model, criterion, optimizer,num_train,gamma,z,epoch):
         B2_var = B2.to(device)
         Y2_var = Y2
  
-        weight = torch.ones(len(B1))
-        weight.to(device) 
+        weight = torch.ones(len(B1),device=device)
         print("---weight is on   ---")
         print(weight.get_device())
         #####compute weights (exp of sum) #######
