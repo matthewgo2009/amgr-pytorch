50bf56930e9553aa483c1f69b24a6375a18efee6
matthewgo2009 <matthewgo2009@gmail.com>
2023-07-12 13:54:49 -0700

    Add files via upload

--- main_amgr.py
+++ main_amgr.py
@@ -201,7 +201,7 @@ def train_v2(train_loader, model, criterion, optimizer, num_train, gamma, z, epo
         # partial_para = params[module.linear.weight]
         ft_compute_grad = grad(compute_loss)
         ft_compute_sample_grad = vmap(ft_compute_grad, in_dims=(None, None, 0, 0))
-        ft_per_sample_grads = ft_compute_sample_grad(params, buffers, input_var, target_var)
+        ft_per_sample_grads = ft_compute_sample_grad(params, buffers, input_var, target_var,criterion)
         print(ft_per_sample_grads.shape)
 
         output = model(input_var)
