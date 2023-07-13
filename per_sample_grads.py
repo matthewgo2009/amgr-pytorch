08c90a347a4b05ce2933049e992eebe06a616866
matthewgo2009 <matthewgo2009@gmail.com>
2023-07-13 13:55:18 -0700

    Update per_sample_grads.py

--- per_sample_grads.py
+++ per_sample_grads.py
@@ -53,7 +53,7 @@ def loss_fn(predictions, targets):
 # Let’s generate a batch of dummy data and pretend that we’re working with an MNIST dataset.
 # The dummy images are 28 by 28 and we use a minibatch of size 64.
 
-device = 'cpu'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 num_models = 10
 batch_size = 64
