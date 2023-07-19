de749a0b71b7ac1c78a3d5c370c5ed31031cd894
matthewgo2009 <matthewgo2009@gmail.com>
2023-07-19 11:26:55 -0700

    Add files via upload

--- main_amgr.py
+++ main_amgr.py
@@ -32,8 +32,8 @@ def main():
     train_loader, val_loader, num_train= utils.get_loaders_v2(args)
 
     num_class = len(args.class_names)
-    # model = torch.nn.DataParallel(resnet32(num_classes=num_class))
-    model = resnet32(num_classes=num_class)
+    model = torch.nn.DataParallel(resnet32(num_classes=num_class))
+    # model = resnet32(num_classes=num_class)
 
     model = model.to(device)
     cudnn.benchmark = True
