55c585565b98da58f9203ee335d167feb26b9985
matthewgo2009 <matthewgo2009@gmail.com>
2023-07-18 21:59:51 -0700

    Add files via upload

--- main_amgr.py
+++ main_amgr.py
@@ -135,7 +135,7 @@ def compute_per_sample_gradients(model, x, target,criterion):
     gradients = []
     def hook_function(module, grad_input, grad_output):
         gradients.append(grad_input[0])
-    hook = model.module.linear.register_backward_hook(hook_function)
+    hook = model.linear.register_backward_hook(hook_function)
 
     # Forward pass
     output = model(x)
