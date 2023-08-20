0cc5b5766bc4ee45f00bc10573c2ba9a0f1f027b
matthewgo2009 <matthewgo2009@gmail.com>
2023-08-20 16:46:42 -0700

    Add files via upload

--- main_amgr.py
+++ main_amgr.py
@@ -79,7 +79,7 @@ def main():
 
             item = input_var[i]
             score[item] = 0
-            class_name = target[i]
+            class_name = int(target[i])
             class_cnt[class_name] += 1
             image_name = 'image/label_'+ str(class_name) + '_' + str(class_cnt[class_name]) +'_'+str(score[item]) + '.png'
             save_image(item, image_name)
