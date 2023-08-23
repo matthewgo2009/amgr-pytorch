114f86ebdde8e8e96b4e83b8b9ff43091d9c49d2
matthewgo2009 <matthewgo2009@gmail.com>
2023-08-23 13:10:46 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -185,7 +185,7 @@ def get_loaders_v2(args):
                             download=True)
     num_train = len(train_dataset)
    
-
+    print(train_dataset[0])
 
     test_dataset = dataset(root=args.data_home,
                            train=False,
