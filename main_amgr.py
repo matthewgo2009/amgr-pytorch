9355a9d90285891e7622ce0c517bb5b0da4e9134
matthewgo2009 <matthewgo2009@gmail.com>
2023-08-23 22:49:08 -0700

    Update main_amgr.py

--- main_amgr.py
+++ main_amgr.py
@@ -316,6 +316,7 @@ def train_v2(train_loader, model, criterion, optimizer, num_train, gamma, z, epo
             elif args.wo == 1:
                 summation = weights.sum()
                 weights = torch.tensor([summation/max(number,args.eps) for number in weights]).to(device)
+                weights = F.softmax(weights)
             weights = weights.detach()
             
             if args.measure == 1:
